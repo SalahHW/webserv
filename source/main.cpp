034b58a3141ee481d9561b96f52c9d2d4637b9f1
Salah <salah.bouheniche@gmail.com>
2024-09-11 03:09:12 +0200

    Refactor main.cpp according to subject

--- source/main.cpp
+++ source/main.cpp
@@ -5,12 +5,35 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/08 13:13:16 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/07/08 13:13:33 by sbouheni         ###   ########.fr       */
+/*   Created: 2024/09/04 02:13:05 by sbouheni          #+#    #+#             */
+/*   Updated: 2024/09/10 15:54:57 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(void)
+#include "webserv.hpp"
+#include <string>
+#include <iostream>
+
+int main(int argc, char **argv)
 {
+	string configFilePath("./config/webserv.conf");
+	if (argc > 2)
+	{
+		cerr << "Usage = ./webserv [config file path]" << endl;
+		return (1);
+	}
+	if (argc == 2)
+		configFilePath = argv[1];
+
+	// cout << configFilePath << endl;
+	try
+	{
+		ConfigFile server_configuration(configFilePath);
+	}
+	catch (runtime_error &error)
+	{
+		cerr << error.what() << endl;
+		return (1);
+	}
 	return (0);
 }
