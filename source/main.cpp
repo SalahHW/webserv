dae4c855c7a4119ceeb36ea084195e6857a20ee4
Salah <salah.bouheniche@gmail.com>
2024-09-12 02:54:23 +0200

    Refactor main.cpp to separate configuration loading into a function

--- source/main.cpp
+++ source/main.cpp
@@ -6,34 +6,34 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 02:13:05 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/09/10 15:54:57 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/09/12 02:17:38 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "webserv.hpp"
-#include <string>
 #include <iostream>
+#include <string>
 
-int main(int argc, char **argv)
+using namespace std;
+
+string loadConfiguration(int argc, char** argv)
 {
-	string configFilePath("./config/webserv.conf");
-	if (argc > 2)
-	{
-		cerr << "Usage = ./webserv [config file path]" << endl;
-		return (1);
-	}
-	if (argc == 2)
-		configFilePath = argv[1];
+    string configFilePath("./config/webserv.conf");
+    if (argc > 2)
+        throw runtime_error("Usage = ./webserv [config file path]");
+    if (argc == 2)
+        configFilePath = argv[1];
+    return (configFilePath);
+}
 
-	// cout << configFilePath << endl;
-	try
-	{
-		ConfigFile server_configuration(configFilePath);
-	}
-	catch (runtime_error &error)
-	{
-		cerr << error.what() << endl;
-		return (1);
-	}
-	return (0);
+int main(int argc, char** argv)
+{
+    try {
+        string configFilePath = loadConfiguration(argc, argv);
+        ConfigFile server_configuration(configFilePath);
+    } catch (runtime_error& error) {
+        cerr << error.what() << endl;
+        return (1);
+    }
+    return (0);
 }
