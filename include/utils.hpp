ae611093807ad0bc7647afaaf927f5ddbac7d83e
Salah <salah.bouheniche@gmail.com>
2024-10-02 09:45:09 +0200

    Refactor include order in utils.hpp

--- include/utils.hpp
+++ include/utils.hpp
@@ -6,20 +6,20 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 14:20:42 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/10/02 09:43:10 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/10/02 09:44:58 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <fstream>
-#include <string>
-#include <cstdlib>
 #include <cerrno>
 #include <climits>
+#include <cstdlib>
+#include <fstream>
+#include <string>
 
 namespace utils {
-	
+
 // File utils
 void openFile(const std::string& fileName, std::ifstream& file);
 
