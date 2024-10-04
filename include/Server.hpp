b182510f02aa2dc0e1ce26fa3d7a86710ed227ee
Salah <salah.bouheniche@gmail.com>
2024-10-04 10:31:16 +0200

    Refactor Server class and add Location support

--- include/Server.hpp
+++ include/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 13:20:46 by joakoeni          #+#    #+#             */
-/*   Updated: 2024/10/03 13:32:05 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/10/04 10:30:12 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,39 @@
 #include <string>
 #include <vector>
 
-class Server {
-private:
-    int listenFd;
-    int port;
-    std::string name;
-    int clientMaxBodySize;
-    std::vector<std::string> ErrorPages;
-    // std::vector<Locationrouteobj> routes;
-    Server& operator=(const Server& src);
-    Server(const Server& src);
+// #include "Location.hpp"
 
+class Location; // TODO: remove after Location.hpp is created
+
+class Server {
 public:
-    Server();
     ~Server();
+    Server();
+
     void setListenFd(int fd);
     void setPort(int port);
     void setCLientMaxBodySize(int size);
     void setName(const std::string& name);
     void setErrorPages(std::vector<std::string> error);
+
+    void addLocation(const Location& location);
+
     int getListenFd() const;
     int getPort() const;
     int getCLientMaxBodySize() const;
     const std::string& getName() const;
     const std::vector<std::string>& getErrorPages() const;
+
+private:
+    Server(const Server& src);
+    Server& operator=(const Server& src);
+    
+    int listenFd;
+    int port;
+    std::string name;
+    int clientMaxBodySize;
+    std::vector<std::string> errorPages; // TODO: Use map instead to store error codes and their corresponding pages
+    std::vector<Location> locations;
 };
 
 #endif
