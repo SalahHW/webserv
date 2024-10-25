8f3ef3f35eb2a9d2e691640716efaf17e387d794
Salah <salah.bouheniche@gmail.com>
2024-10-25 10:01:04 +0200

    Refactor ConfigExtractor to create a map of int/Server instead of vector

--- source/parser/ConfigExtractor.cpp
+++ source/parser/ConfigExtractor.cpp
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 08:10:24 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/10/18 21:07:02 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/10/25 09:55:02 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 
 // ConfigExtractor::ConfigExtractor() { }
 
-std::vector<Server> ConfigExtractor::extractServers(const ConfigFile& configFile)
+std::map<int, Server> ConfigExtractor::extractServers(const ConfigFile& configFile)
 {
-	std::vector<Server> servers;
+	std::map<int, Server> servers;
 	const Block& mainBlock = configFile.getMainBlock();
 	const std::vector<Block*> subBlocks = mainBlock.getSubBlocks();
 
@@ -28,7 +28,8 @@ std::vector<Server> ConfigExtractor::extractServers(const ConfigFile& configFile
 			Server server;
 			extractServerDirectives(block, server);
 			extractLocationBlocks(block, server);
-			servers.push_back(server);
+			std::cout << server.getListenFd() << std::endl;
+			servers[server.getListenFd()] = server;
 		}
 	}
 	return servers;
