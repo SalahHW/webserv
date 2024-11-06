4bb349ffd442b47762504ae2356289c8d0227688
Salah <salah.bouheniche@gmail.com>
2024-11-06 20:12:11 +0100

    refactor: Enhance LimitExceptBlock to handle HTTP method arguments and improve validation

--- source/block/LimitExceptBlock.cpp
+++ source/block/LimitExceptBlock.cpp
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 11:09:17 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/11/05 14:23:56 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/11/06 20:08:42 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,49 @@
 LimitExceptBlock::~LimitExceptBlock() { }
 
 LimitExceptBlock::LimitExceptBlock(const std::string& fullBlockLine, Block* contextBlock)
-	: Block(fullBlockLine, contextBlock)
+    : Block(fullBlockLine, contextBlock)
 {
-	addValidContext("location");
+    addValidContext("location");
     minArgs = 1;
-	maxArgs = 3;
+    maxArgs = 3;
+
+    isGetConcerned = false;
+    isPostConcerned = false;
+    isDeleteConcerned = false;
+    validate();
 }
 
 LimitExceptBlock::LimitExceptBlock(const LimitExceptBlock& other)
-	: Block(other)
+    : Block(other)
 {
 }
 
 LimitExceptBlock& LimitExceptBlock::operator=(const LimitExceptBlock& other)
 {
-	if (this != &other) {
-		Block::operator=(other);
-	}
-	return *this;
+    if (this != &other) {
+        Block::operator=(other);
+    }
+    return *this;
 }
 
 bool LimitExceptBlock::validateSpecific()
 {
-	// Placeholder implementation, update as needed
-	return true;
+    return extractArguments();
+}
+
+bool LimitExceptBlock::extractArguments()
+{
+    for (size_t i = 0; i < arguments.size(); i++) {
+        if (arguments[i] == "GET") {
+            isGetConcerned = true;
+        } else if (arguments[i] == "POST") {
+            isPostConcerned = true;
+        } else if (arguments[i] == "DELETE") {
+            isDeleteConcerned = true;
+        } else {
+            std::cerr << "Error: " << "limit_expect block has an invalid argument \"" << arguments[i] << "\"" << std::endl;
+            return false;
+        }
+    }
+    return true;
 }
