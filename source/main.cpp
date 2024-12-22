cbcc2e440091d9635390d7c69d91386bbbc07bde
Salah <salah.bouheniche@gmail.com>
2024-12-22 22:33:07 +0100

    feat: refactor main function to improve configuration loading and error handling

--- source/main.cpp
+++ source/main.cpp
@@ -1,43 +1,35 @@
 #include <iostream>
 #include <string>
 
-#include "ConfigExtractor.hpp"
 #include "ConfigFile.hpp"
 #include "Port.hpp"
 #include "ServerHandler.hpp"
 
-std::string loadConfiguration(int argc, char** argv)
-{
-    std::string configFilePath("./config/webserv.conf");
-    if (argc > 2)
-        throw std::runtime_error("Usage = ./webserv [config file path]");
-    if (argc == 2)
-        configFilePath = argv[1];
-    return (configFilePath);
-}
-
 int main(int argc, char** argv)
 {
-    std::string configFilePath = loadConfiguration(argc, argv);
+    std::string configFilePath("./config/webserv.conf");
+    if (argc > 2) {
+        std::cerr << "Usage = ./webserv [config file path]" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    if (argc == 2)
+        configFilePath = argv[1];
+
     ConfigFile server_configuration(configFilePath);
     if (!server_configuration.good()) {
         std::cerr << "Error: Invalid configuration file" << std::endl;
         return (1);
     }
 
-    std::vector<Server> servers = ConfigExtractor::extractServers(server_configuration);
-    // Configfinalizer verifie que tout les ports sont OK et finalise la configuration
-    // Le premier serveur est le serveur par defaut pour ce port sauf si defaut_server est specifier dans un autre serveur
-    // ConfigFinalizer ==> Liste de port
-    ConfigFinalizer configFinalizer;
-    configFinalizer.finalizeConfig(servers);
-    std::map<int, Port> ports = configFinalizer.getPorts();
+    //TODO: Update Configuration to setup  a default server for a port
+
+    std::map<int, Port> ports = server_configuration.getPortMap();
     std::map<int, Port>::const_iterator itPort;
 
     for (itPort = ports.begin(); itPort != ports.end(); ++itPort) {
         itPort->second.displayHosts();
     }
-
+    
     // ServerHandler serverHandler(ports);
     return (0);
 }
