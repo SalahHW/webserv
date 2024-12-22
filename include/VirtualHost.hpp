2b6fe1fb8032b6058008d4f2eedaa9965680ae85
Salah <salah.bouheniche@gmail.com>
2024-12-22 15:21:17 +0100

    feat: enhance VirtualHost class with additional member functions and improved constructor

--- include/VirtualHost.hpp
+++ include/VirtualHost.hpp
@@ -1,26 +1,29 @@
 #pragma once
 
-#include <string>
-#include <map>
 #include "Location.hpp"
+#include <map>
+#include <string>
 
 class VirtualHost {
-    public:
-        VirtualHost();
-        ~VirtualHost();
-        VirtualHost(const VirtualHost &src);
-        VirtualHost &operator=(const VirtualHost &src);
+public:
+    ~VirtualHost();
+    VirtualHost();
+    VirtualHost(const VirtualHost& src);
+    VirtualHost& operator=(const VirtualHost& src);
 
-        void setClientMaxBodySize(int clientMaxBodySize);
-        void setErrorPage(const std::map<int, std::string>& errorPages);
-        void setLocation(const std::map<std::string, Location>& locations);
-        
-        int getClientMaxBodySize() const;
-        const std::map<int, std::string>& getErrorPages() const;
-        const std::map<std::string, Location>& getLocations() const;
+    void setName(const std::string& name);
+    void setClientMaxBodySize(int clientMaxBodySize);
+    void setErrorPages(const std::map<int, std::string>& errorPages);
+    void setLocations(const std::map<std::string, Location>& locations);
 
-    private:
-        int clientMaxBodySize;
-        std::map<int, std::string> errorPages;
-        std::map<std::string, Location> locations;
+    const std::string& getName() const;
+    int getClientMaxBodySize() const;
+    const std::map<int, std::string>& getErrorPages() const;
+    const std::map<std::string, Location>& getLocations() const;
+
+private:
+    std::string name;
+    int clientMaxBodySize;
+    std::map<int, std::string> errorPages;
+    std::map<std::string, Location> locations;
 };
--- source/server/VirtualHost.cpp
+++ source/server/VirtualHost.cpp
@@ -1,44 +1,67 @@
-#include "VirtualHost.h"
+#include "VirtualHost.hpp"
 
-VirtualHost::VirtualHost() {
+VirtualHost::~VirtualHost()
+{
 }
 
-VirtualHost::~VirtualHost() {
+VirtualHost::VirtualHost()
+    : name("")
+    , clientMaxBodySize(-1)
+{
 }
 
-VirtualHost::VirtualHost(const VirtualHost& other) {
-    this->clientMaxBodySize = other.clientMaxBodySize;
-    this->errorPages = other.errorPages;
-    this->locations = other.locations;
+VirtualHost::VirtualHost(const VirtualHost& other)
+{
+    *this = other;
 }
 
-VirtualHost& VirtualHost::operator=(const VirtualHost& other) {
-    this->clientMaxBodySize = other.clientMaxBodySize;
-    this->errorPages = other.errorPages;
-    this->locations = other.locations;
+VirtualHost& VirtualHost::operator=(const VirtualHost& other)
+{
+    if (this != &other) {
+        this->name = other.name;
+        this->clientMaxBodySize = other.clientMaxBodySize;
+        this->errorPages = other.errorPages;
+        this->locations = other.locations;
+    }
     return *this;
 }
 
-void VirtualHost::setClientMaxBodySize(const std::string& clientMaxBodySize) {
+void VirtualHost::setName(const std::string& name)
+{
+    this->name = name;
+}
+
+void VirtualHost::setClientMaxBodySize(int clientMaxBodySize)
+{
     this->clientMaxBodySize = clientMaxBodySize;
 }
 
-void VirtualHost::setErrorPages(const std::map<int, std::string>& errorPages) {
+void VirtualHost::setErrorPages(const std::map<int, std::string>& errorPages)
+{
     this->errorPages = errorPages;
 }
 
-void VirtualHost::setLocations(const std::map<std::string, Location>& locations) {
+void VirtualHost::setLocations(const std::map<std::string, Location>& locations)
+{
     this->locations = locations;
 }
 
-const std::string& VirtualHost::getClientMaxBodySize() const {
+const std::string& VirtualHost::getName() const
+{
+    return name;
+}
+
+int VirtualHost::getClientMaxBodySize() const
+{
     return clientMaxBodySize;
 }
 
-const std::map<int, std::string>& VirtualHost::getErrorPages() const {
+const std::map<int, std::string>& VirtualHost::getErrorPages() const
+{
     return errorPages;
 }
 
-const std::map<std::string, Location>& VirtualHost::getLocations() const {
+const std::map<std::string, Location>& VirtualHost::getLocations() const
+{
     return locations;
 }
