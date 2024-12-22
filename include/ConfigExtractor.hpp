ad7242dcdb5775668f6203773c389f8d41df2f29
Salah <salah.bouheniche@gmail.com>
2024-12-22 22:25:53 +0100

    feat: add getPortMap method to ConfigExtractor for port retrieval from configuration

--- include/ConfigExtractor.hpp
+++ include/ConfigExtractor.hpp
@@ -1,21 +1,20 @@
 #pragma once
 
 #include <map>
+#include <vector>
 
 #include "ConfigFile.hpp"
+#include "ConfigFinalizer.hpp"
+#include "Port.hpp"
 #include "Server.hpp"
 
+class ConfigFile;
+
 class ConfigExtractor {
- public:
-  // ~ConfigExtractor();
-  // ConfigExtractor();
-
-  static std::vector<Server> extractServers(const ConfigFile& configFile);
-  static void extractServerDirectives(const Block& block, Server& server);
-  static void extractLocationBlocks(const Block& block, Server& server);
-  static void extractLocationDirectives(const Block& block, Location& location);
-
-  // private:
-  //     ConfigExtractor(const ConfigExtractor& other);
-  //     ConfigExtractor& operator=(const ConfigExtractor& other);
+public:
+    static std::vector<Server> extractServers(const ConfigFile& configFile);
+    static void extractServerDirectives(const Block& block, Server& server);
+    static void extractLocationBlocks(const Block& block, Server& server);
+    static void extractLocationDirectives(const Block& block, Location& location);
+    static std::map<int, Port> getPortMap(const ConfigFile& configFile);
 };
--- source/parser/ConfigExtractor.cpp
+++ source/parser/ConfigExtractor.cpp
@@ -1,49 +1,62 @@
 #include "ConfigExtractor.hpp"
 
 std::vector<Server> ConfigExtractor::extractServers(
-    const ConfigFile &configFile) {
-  std::vector<Server> servers;
-  const Block &mainBlock = configFile.getMainBlock();
-  const std::vector<Block *> subBlocks = mainBlock.getSubBlocks();
+    const ConfigFile& configFile)
+{
+    std::vector<Server> servers;
+    const Block& mainBlock = configFile.getMainBlock();
+    const std::vector<Block*> subBlocks = mainBlock.getSubBlocks();
 
-  for (size_t i = 0; i < subBlocks.size(); i++) {
-    const Block &block = *subBlocks[i];
-    if (block.getName() == "server") {
-      Server server;
-      extractServerDirectives(block, server);
-      extractLocationBlocks(block, server);
-      servers.push_back(server);
+    for (size_t i = 0; i < subBlocks.size(); i++) {
+        const Block& block = *subBlocks[i];
+        if (block.getName() == "server") {
+            Server server;
+            extractServerDirectives(block, server);
+            extractLocationBlocks(block, server);
+            servers.push_back(server);
+        }
     }
-  }
-  return servers;
+    return servers;
 }
 
-void ConfigExtractor::extractServerDirectives(const Block &block,
-                                              Server &server) {
-  const std::vector<Directive *> directives = block.getDirectives();
-  for (size_t i = 0; i < directives.size(); ++i) {
-    directives[i]->apply(server);
-  }
-}
-
-void ConfigExtractor::extractLocationBlocks(const Block &block,
-                                            Server &server) {
-  const std::vector<Block *> &subBlocks = block.getSubBlocks();
-  for (size_t i = 0; i < subBlocks.size(); ++i) {
-    const Block &subBlock = *subBlocks[i];
-    if (subBlock.getName() == "location") {
-      Location location;
-      subBlock.apply(location);
-      extractLocationDirectives(subBlock, location);
-      server.addLocation(location);
+void ConfigExtractor::extractServerDirectives(const Block& block,
+    Server& server)
+{
+    const std::vector<Directive*> directives = block.getDirectives();
+    for (size_t i = 0; i < directives.size(); ++i) {
+        directives[i]->apply(server);
     }
-  }
 }
 
-void ConfigExtractor::extractLocationDirectives(const Block &block,
-                                                Location &location) {
-  const std::vector<Directive *> directives = block.getDirectives();
-  for (size_t i = 0; i < directives.size(); ++i) {
-    directives[i]->apply(location);
-  }
+void ConfigExtractor::extractLocationBlocks(const Block& block,
+    Server& server)
+{
+    const std::vector<Block*>& subBlocks = block.getSubBlocks();
+    for (size_t i = 0; i < subBlocks.size(); ++i) {
+        const Block& subBlock = *subBlocks[i];
+        if (subBlock.getName() == "location") {
+            Location location;
+            subBlock.apply(location);
+            extractLocationDirectives(subBlock, location);
+            server.addLocation(location);
+        }
+    }
+}
+
+void ConfigExtractor::extractLocationDirectives(const Block& block,
+    Location& location)
+{
+    const std::vector<Directive*> directives = block.getDirectives();
+    for (size_t i = 0; i < directives.size(); ++i) {
+        directives[i]->apply(location);
+    }
+}
+
+std::map<int, Port> ConfigExtractor::getPortMap(const ConfigFile& configFile)
+{
+    ConfigFinalizer finalizer;
+    std::vector<Server> servers = extractServers(configFile);
+    finalizer.finalizeConfig(servers);
+
+    return finalizer.getPorts();
 }
