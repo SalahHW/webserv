76462660078c787761d233a51b076a043d1d1c61
Salah <salah.bouheniche@gmail.com>
2025-01-27 07:30:19 +0100

    feat: add ClientBodyTempPathDirective class for managing temporary path settings

---
+++ include/ClientBodyTempPathDirectice.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Directive.hpp"
+
+class ClientBodyTempPathDirective : public Directive {
+public:
+    ~ClientBodyTempPathDirective();
+    ClientBodyTempPathDirective(Block* currentContext, const std::string& fullDirectiveLine);
+    ClientBodyTempPathDirective(const ClientBodyTempPathDirective& other);
+    ClientBodyTempPathDirective& operator=(const ClientBodyTempPathDirective& other);
+
+    virtual bool validateSpecific();
+    virtual void displayInfo() const;
+    virtual void apply(Server& server);
+    virtual void apply(Location& location);
+
+    void setTempPath(std::string path);
+
+    std::string getTempPath() const;
+
+private:
+    ClientBodyTempPathDirective();
+
+    std::string tempPath;
+};
