0b5ccaf38ad4295658f55ba2cbc15fccce18601f
Salah <salah.bouheniche@gmail.com>
2025-01-29 22:45:35 +0100

    refactor: improve code formatting and consistency in IndexDirective class

--- source/directive/IndexDirective.cpp
+++ source/directive/IndexDirective.cpp
@@ -1,10 +1,11 @@
 #include "IndexDirective.hpp"
 
-IndexDirective::~IndexDirective() {}
+IndexDirective::~IndexDirective() { }
 
-IndexDirective::IndexDirective(Block *currentContext,
-                               const std::string &fullDirectiveLine)
-    : Directive(currentContext, fullDirectiveLine) {
+IndexDirective::IndexDirective(Block* currentContext,
+    const std::string& fullDirectiveLine)
+    : Directive(currentContext, fullDirectiveLine)
+{
   setName("index");
   setMinArgs(1);
   setMaxArgs(1);
@@ -12,34 +13,43 @@ IndexDirective::IndexDirective(Block *currentContext,
   validate();
 }
 
-IndexDirective::IndexDirective(const IndexDirective &other)
-    : Directive(other), indexPath(other.indexPath) {}
+IndexDirective::IndexDirective(const IndexDirective& other)
+    : Directive(other)
+    , indexPath(other.indexPath)
+{
+}
 
-IndexDirective &IndexDirective::operator=(const IndexDirective &other) {
-  if (this != &other) {
+IndexDirective& IndexDirective::operator=(const IndexDirective& other)
+{
+  if (this != &other)
+  {
     Directive::operator=(other);
     indexPath = other.indexPath;
   }
   return *this;
 }
 
-bool IndexDirective::validateSpecific() {
+bool IndexDirective::validateSpecific()
+{
   indexPath = getArguments()[0];
   // TODO: Implement validation
   return true;
 }
 
-void IndexDirective::displayInfo() const {
+void IndexDirective::displayInfo() const
+{
   std::cout << "Name : " << this->getName() << std::endl
             << "- index path : " << indexPath << std::endl;
 }
 
-void IndexDirective::apply(Location &location) {
+void IndexDirective::apply(Location& location)
+{
   location.setIndexFile(indexPath);
 }
 
-void IndexDirective::setIndexPath(const std::string &indexPath) {
+void IndexDirective::setIndexPath(const std::string& indexPath)
+{
   this->indexPath = indexPath;
 }
 
-const std::string &IndexDirective::getIndexPath() const { return indexPath; }
+const std::string& IndexDirective::getIndexPath() const { return indexPath; }
