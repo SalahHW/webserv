c53880bcf2df699681dba57ee809971513bee3eb
Salah <salah.bouheniche@gmail.com>
2025-01-29 22:44:24 +0100

    feat: add client body temporary path management to VirtualHost class

--- include/VirtualHost.hpp
+++ include/VirtualHost.hpp
@@ -13,17 +13,20 @@ class VirtualHost {
   VirtualHost& operator=(const VirtualHost& src);
 
   void setName(const std::string& name);
+  void setClientBodyTempPath(const std::string& path);
   void setClientMaxBodySize(int clientMaxBodySize);
   void setErrorPages(const std::map<size_t, std::string>& errorPages);
   void setLocations(const std::map<std::string, Location>& locations);
 
   const std::string& getName() const;
+  const std::string& getClientBodyTempPath() const;
   int getClientMaxBodySize() const;
   const std::map<size_t, std::string>& getErrorPages() const;
   const std::map<std::string, Location>& getLocations() const;
 
  private:
   std::string name;
+  std::string clientBodyTempPath;
   int clientMaxBodySize;
   std::map<size_t, std::string> errorPages;
   std::map<std::string, Location> locations;
--- source/server/VirtualHost.cpp
+++ source/server/VirtualHost.cpp
@@ -1,14 +1,23 @@
 #include "VirtualHost.hpp"
 
-VirtualHost::~VirtualHost() {}
+VirtualHost::~VirtualHost() { }
 
-VirtualHost::VirtualHost() : name(""), clientMaxBodySize(-1) {}
+VirtualHost::VirtualHost()
+    : clientMaxBodySize(-1)
+{
+}
 
-VirtualHost::VirtualHost(const VirtualHost& other) { *this = other; }
+VirtualHost::VirtualHost(const VirtualHost& other)
+{
+  *this = other;
+}
 
-VirtualHost& VirtualHost::operator=(const VirtualHost& other) {
-  if (this != &other) {
+VirtualHost& VirtualHost::operator=(const VirtualHost& other)
+{
+  if (this != &other)
+  {
     this->name = other.name;
+    this->clientBodyTempPath = other.clientBodyTempPath;
     this->clientMaxBodySize = other.clientMaxBodySize;
     this->errorPages = other.errorPages;
     this->locations = other.locations;
@@ -16,33 +25,47 @@ VirtualHost& VirtualHost::operator=(const VirtualHost& other) {
   return *this;
 }
 
-void VirtualHost::setName(const std::string& name) {
+void VirtualHost::setName(const std::string& name)
+{
   this->name = name;
-  std::cout << "VirtualHost::setName: " << name << std::endl;
 }
 
-void VirtualHost::setClientMaxBodySize(int clientMaxBodySize) {
+void VirtualHost::setClientBodyTempPath(const std::string& path)
+{
+  this->clientBodyTempPath = path;
+}
+
+void VirtualHost::setClientMaxBodySize(int clientMaxBodySize)
+{
   this->clientMaxBodySize = clientMaxBodySize;
 }
 
-void VirtualHost::setErrorPages(
-    const std::map<size_t, std::string>& errorPages) {
+void VirtualHost::setErrorPages(const std::map<size_t, std::string>& errorPages)
+{
   this->errorPages = errorPages;
 }
 
-void VirtualHost::setLocations(
-    const std::map<std::string, Location>& locations) {
+void VirtualHost::setLocations(const std::map<std::string, Location>& locations)
+{
   this->locations = locations;
 }
 
-const std::string& VirtualHost::getName() const { return name; }
+const std::string& VirtualHost::getName() const
+{
+  return name;
+}
 
-int VirtualHost::getClientMaxBodySize() const { return clientMaxBodySize; }
+int VirtualHost::getClientMaxBodySize() const
+{
+  return clientMaxBodySize;
+}
 
-const std::map<size_t, std::string>& VirtualHost::getErrorPages() const {
+const std::map<size_t, std::string>& VirtualHost::getErrorPages() const
+{
   return errorPages;
 }
 
-const std::map<std::string, Location>& VirtualHost::getLocations() const {
+const std::map<std::string, Location>& VirtualHost::getLocations() const
+{
   return locations;
 }
